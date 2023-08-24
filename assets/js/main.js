35918fd79ec07d43555255d1a027543e2f5a4239
AntoninoCucuzza <cucuzza.antonino@hotmail.com>
2023-08-24 15:40:58 +0200

    milestone 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,4 +8,33 @@ il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 
+MILESTONE 1: Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra.
+La risposta finale (o output) sarà anch’essa da scrivere in solo console.
+
+MILESTONE 2:
+Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
+Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
+Questo richiederà un minimo di ricerca.
 */
+
+console.log('#############');
+
+const priceKm = (0.21);
+console.log(priceKm);
+
+var inputKm = document.getElementById('nkm'); //prendo il nome dall'input nkm
+
+var inputAge = document.getElementById('age'); //prendo l'eta dall'input age
+
+document.querySelector('.dati').addEventListener('submit', function(e){
+    e.preventDefault(); 
+    
+    console.log (Number(inputAge.value));    
+    console.log(Number(inputKm.value));    
+    
+   // var priceTot = (inputKm * priceKm);   document.getElementById(nkm)  
+    console.log (priceTot);
+
+});
+
+
